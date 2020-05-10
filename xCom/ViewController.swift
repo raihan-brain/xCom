88fc7d48a108a3f987abf1165dd454f7030be005
raihan-brain <raihanuzzaman@bs-23.net>
2020-05-10 17:31:58 +0600

    Initial Commit

---
+++ xCom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xCom
+//
+//  Created by Shan Zaman on 10/5/20.
+//  Copyright © 2020 Shan Zaman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
