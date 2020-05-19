532f877deea113bf01d42a36739d40f7b63e2fef
raihan zaman <raihanuzzaman@bs-23.net>
2020-05-19 20:22:23 +0600

    added navigation

--- xCom/controllers/SignupViewController.swift
+++ xCom/controllers/SignupViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class SignupViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var phoneTextField: UITextField!
@@ -17,9 +17,10 @@ class SignupViewController: UIViewController {
     @IBOutlet weak var confirmPasswordTextField: UITextField!
     @IBOutlet weak var signupButton: UIButton!
     
+    var destinationVC: UIViewController?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         modifyUi()
     }
@@ -33,15 +34,26 @@ class SignupViewController: UIViewController {
         signupButton.makeCornerRound(uiButton: signupButton)
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    @IBAction func signupButtonPressed(_ sender: UIButton) {
+        if let vc = UIStoryboard(name: "Auth", bundle: nil).instantiateViewController(withIdentifier: "LoginVC") as? LoginViewController {
+            vc.modalTransitionStyle = .crossDissolve
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+        
+        
+        
     }
-    */
-
+    
+    
 }
